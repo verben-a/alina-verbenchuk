de636a028c7a0ec2f87a1aa3442b9f3078b7c304
verben-a <alina.verbenchuk@gmail.com>
2018-02-16 22:33:46 +0000

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MDBBootstrapModule.forRoot(),
+    AppRoutingModule
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
